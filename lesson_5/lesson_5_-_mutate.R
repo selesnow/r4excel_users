7ee83a01fc25ce0fd0d6d3e13d2b95028317f500
Fasdan <45488035+Fasdan@users.noreply.github.com>
2020-08-19 19:38:57 +0300

    Update lesson_5_-_mutate.R

--- lesson_5/lesson_5_-_mutate.R
+++ lesson_5/lesson_5_-_mutate.R
@@ -8,24 +8,24 @@ ga_data <- vroom("https://raw.githubusercontent.com/selesnow/publications/master
 # mutate
 # добавл¤ем новый столбец
 ga_data <- mutate(ga_data,
-                  bounce_rate = bounces / sessions)
+                  bounce_rate = bounces / sessions) #новый столбец = формула
 
 ga_data <- mutate(ga_data,
                   bounce_rate = bounces / sessions,
-                  br_group    = if_else(bounce_rate > 0.6, "high_br", "normal_br"))
+                  br_group    = if_else(bounce_rate > 0.6, "high_br", "normal_br")) #такое же строение как функции в excel
 
 # примен¤ем преобразование к уже существующим столбцам
-ga_data %>% mutate_if(is.character, toupper)
+ga_data %>% mutate_if(is.character, toupper) #меняет регистр данных в столбце
 
 # преобразуем значени¤ существующих столбцов примен¤¤ регул¤рные выражени¤
-ga_data %>% mutate_at(vars(matches("s$")), sqrt )
+ga_data %>% mutate_at(vars(matches("s$")), sqrt ) #заменяет данные в столбцах на их квадратный корень
 
 # transemute
 # убираем все столбцы кроме преобразованных
 transmute(ga_data,
           bounce_rate = bounces / sessions,
           date        = format(date, "%d %B %Y"),
-          source)
+          source) #преобразования + убирание лишних столбцов
 
 # преобразовываем столбцы по их признаку и удал¤ем все остальные
 transmute_if(ga_data, 
