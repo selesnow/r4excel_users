e2c00b105f3c078714d31219d732025296aa5138
Alexey Seleznev <selesnow@mail.ru>
2022-08-12 10:25:20 +0300

    1. Добавил в видео поддержку JS API, для отслеживания просмотров в GA 2. Залил весь проект книги на github

---
+++ js_tests/1.js
@@ -0,0 +1,17 @@
+<!-- Online Test Pad Test Widget (#101485)-->
+<div id="otp_wgt_fqqn45fe7pm5q"></div>
+<script type="text/javascript">
+    var otp_wjs_dt = (new Date).getTime();
+    (function (w, d, n, s, rp) {
+        w[n] = w[n] || [];
+        rp = {};
+        w[n].push(function () {
+            otp_render_widget(d.getElementById("otp_wgt_fqqn45fe7pm5q"), 'onlinetestpad.com', 'fqqn45fe7pm5q', rp);
+        }); 
+        s = d.createElement("script");
+        s.type = "text/javascript";
+        s.src = "//onlinetestpad.com/js/widget.js?" + otp_wjs_dt;
+        s.async = true;
+        d.getElementsByTagName("head")[0].appendChild(s);
+    })(this, this.document, "otp_widget_callbacks");
+</script>
